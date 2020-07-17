148874f06f4124bd60c6d0cd594c769dba71998e
kelvin53 <geshkoskei53@gmail.com>
2020-07-17 16:39:10 +0300

    modify views.py

--- views.py
+++ views.py
@@ -3,10 +3,35 @@ from newsapi import NewsApiClient
 
 app = Flask(__name__)
 
+@app.route('/')
+def Index():
+    newsapi = NewsApiClient(api_key="bbb081415b6b417eb5d3fbfe57199cf8")
+    topheadlines = newsapi.get_top_headlines(sources="abc-news")
 
+    articles = topheadlines['articles']
+
+    desc = []
+    news = []
+    img = []
+    url = []
+    publAt = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+        url.append(myarticles['url'])
+        publAt.append(myarticles['publishedAt'])
+        
+
+    mylist = zip(news, desc, img,url,publAt)
+
+    return render_template('index.html', context = mylist)
 @app.route('/bbc')
 def bbc():
-    newsapi = NewsApiClient(api_key="e8902ba0f9454263b59e19a6f175c99c")
+    newsapi = NewsApiClient(api_key="bbb081415b6b417eb5d3fbfe57199cf8")
     topheadlines = newsapi.get_top_headlines(sources="bbc-news")
 
     articles = topheadlines['articles']
@@ -33,7 +58,7 @@ def bbc():
     
 @app.route('/fox')
 def fox():
-    newsapi = NewsApiClient(api_key="e8902ba0f9454263b59e19a6f175c99c")
+    newsapi = NewsApiClient(api_key="bbb081415b6b417eb5d3fbfe57199cf8")
     topheadlines = newsapi.get_top_headlines(sources="fox-news")
 
     articles = topheadlines['articles']
@@ -59,7 +84,7 @@ def fox():
     return render_template('fox.html', context = mylist)
 @app.route('/nbc')
 def nbc():
-    newsapi = NewsApiClient(api_key="e8902ba0f9454263b59e19a6f175c99c")
+    newsapi = NewsApiClient(api_key="bbb081415b6b417eb5d3fbfe57199cf8")
     topheadlines = newsapi.get_top_headlines(sources="nbc-news")
 
     articles = topheadlines['articles']
