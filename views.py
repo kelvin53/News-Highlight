e1fbf5eee05bf54322388b8535fc09294ec3c21e
kelvin53 <geshkoskei53@gmail.com>
2020-07-17 16:32:44 +0300

    modify views module

--- views.py
+++ views.py
@@ -3,32 +3,7 @@ from newsapi import NewsApiClient
 
 app = Flask(__name__)
 
-@app.route('/')
-def Index():
-    newsapi = NewsApiClient(api_key="e8902ba0f9454263b59e19a6f175c99c")
-    topheadlines = newsapi.get_top_headlines(sources="bitcoin.com")
 
-    articles = topheadlines['articles']
-
-    desc = []
-    news = []
-    img = []
-    url = []
-    publAt = []
-
-    for i in range(len(articles)):
-        myarticles = articles[i]
-
-        news.append(myarticles['title'])
-        desc.append(myarticles['description'])
-        img.append(myarticles['urlToImage'])
-        url.append(myarticles['url'])
-        publAt.append(myarticles['publishedAt'])
-        
-
-    mylist = zip(news, desc, img,url,publAt)
-
-    return render_template('index.html', context = mylist)
 @app.route('/bbc')
 def bbc():
     newsapi = NewsApiClient(api_key="e8902ba0f9454263b59e19a6f175c99c")
